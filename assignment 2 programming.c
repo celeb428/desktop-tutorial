527391126c456f3f611d36110cd16c8abf6d187c
celeb428 <mburu.26188@students.kyu.ac.ke>
2025-09-18 10:16:35 +0300

    Add files via upload
    add my assignment

---
+++ assignment 2 programming.c
@@ -0,0 +1,43 @@
+//SIMPLE C PROGRAM (APPLICATION OF CLINICAL CHECK UP )
+/*
+name:caleb ngugi mburu
+Reg:ct100/G/26188/25
+Description:APPLICATION OF CLINICAL CHECK UP
+*/
+
+#include <stdio.h>
+
+//main function
+int main(){
+
+	
+	int age;
+     printf("enter your age(in years) :");
+     scanf("%d",&age);
+     printf("your age is %d \n",age);
+     
+     int height;
+     printf("enter your height(in centimetres) :");
+     scanf("%d",&height);
+     printf("your height is %d\n",height);
+     
+     int weight;
+     printf("enter your weight (in kilograms):");
+     scanf("%d",&weight);
+     printf("your weight is %d\n",weight);
+     
+     int  number;
+     printf("enter your phone number(in numbers):");
+     scanf("%d",& number);
+     printf("your phone number is %d\n", number);
+     
+     
+     
+     int name ;
+     printf("enter your name :");
+     scanf("%d",&name);
+     printf("your name is %d\n",name);
+     
+	return 0;
+}	
+	
