2ef046acef63662e82510c02c31bbef613436853
CT100/G/26188/25 CALEB <mburu.26188@students.kyu.ac.ke>
2025-10-09 10:45:15 +0300

    Add files via upload
    order of numbers

---
+++ number_linnning.c
@@ -0,0 +1,18 @@
+//simple c program
+/*
+name:caleb ngugi mburu
+reg:ct100/g/26188/25
+date:07/10/2025
+description:numbers from 100 to 50
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 100; i >= 50; i--) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
